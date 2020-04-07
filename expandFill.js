30a4e0863a0d49773f0fd5e566707eb4c05b98e0
Nichole <nichole.king94@hotmail.com>
2020-04-07 09:46:51 -0400

    First commit

---
+++ expandFill.js
@@ -0,0 +1,8 @@
+//Expand a selection by a certain pixel amount and fill it with the foreground color
+var expandAmount = prompt("Expand amount: ", 2);
+if(isNaN(expandAmount)){
+	alert("Error, please enter a number");
+} else {
+	app.activeDocument.selection.expand(expandAmount);
+	app.activeDocument.selection.fill(app.foregroundColor);	
+}
